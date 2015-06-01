cdd711763c0b31058a346c61bfb8e3a54c70c9f3
Aldy Wirawan <aldy_wirawan@live.com>
2015-06-01 10:51:24 +0700

    init new project

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('index_simon');
+});
+
+Route::get('simon', function () {
+    return view('index_simon');
+});
+
+//Route::resource('donatur', 'DonaturController');
+//Route::resource('donasi', 'DonasiController');
+
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
